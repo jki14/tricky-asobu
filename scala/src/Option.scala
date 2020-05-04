aff5ea53a0620a53247b7830770f5136c32feae8
jki14 <jki14wz@gmail.com>
2020-05-04 11:22:11 +0900

    Play with Scala
    Play with Scala Option.

--- scala/src/Option.scala
+++ scala/src/Option.scala
@@ -1,12 +1,13 @@
-val nullx = None: Option[Long]
-val nully = None: Option[Long]
-var seven = None: Option[Long]
+val nullx = None: Option[Int]
+val nully = None: Option[Int]
+var seven = None: Option[Int]
 
 seven = Some(7)
 
 println(7 + nullx.getOrElse(nully.getOrElse(seven.get)))
 
 val fourt :Int = 14
+println(nullx)
 println(nullx.getOrElse(fourt))
 println(seven.getOrElse(fourt))
 println(nullx.fold("")(_.toString))
@@ -14,6 +15,10 @@ println(seven.fold("")(_.toString))
 println(nully.toString)
 println(seven.fold(0)(_.toInt))
 println(nully.fold(0)(_.toInt))
+
+def inc(x: Int) = x + 1
+println(nullx.fold(0)(inc))
+println(seven.fold(0)(inc))
 
 val foo = fourt - seven.fold(fourt)(_.toInt)
 
