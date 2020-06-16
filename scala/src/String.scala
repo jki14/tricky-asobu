a0ada0c229c804a8db78c1d4a3c71eafb4b18b97
jki14 <jki14wz@gmail.com>
2020-06-16 21:21:07 +0900

    Play with Scala
    Play with Scala String.

--- scala/src/String.scala
+++ scala/src/String.scala
@@ -3,3 +3,13 @@ println(s"Result: ${foo.mkString(",")}")
 
 val bar = s"Hello World".split(' ').reverse.mkString("++")
 println(bar)
+
+println({
+    val foo = s"nlb-in-Z4I4-abc.xyz.com"
+    foo.split('.')(0).split('-').reverse(1)
+})
+
+println({
+    val foo = s"nlb-in-Z4I4-abc"
+    foo.split('.')(0).split('-').reverse(1)
+})
