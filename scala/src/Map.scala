c31a4dd487ec7d51f05738de24dfb76515c262bf
jki14 <jki14wz@gmail.com>
2020-07-07 21:14:01 +0900

    Play with Scala
    Play with Map & Set.

--- scala/src/Map.scala
+++ scala/src/Map.scala
@@ -5,3 +5,13 @@ val foo : Seq[String] = Seq(s"Hello", s"World")
 val bar = foo.map(mss)
 
 println(bar.reverse.mkString(" "))
+
+var map : Map[String, Set[String]] = Map.empty
+map = map + ("CORP" -> Set[String]("X", "A", "Y"))
+map = map + ("PROD" -> Set[String]("A", "Z", "Y"))
+
+println(map.mkString("\n"))
+
+var insect : Set[String] = Set("U")
+insect ++= map("CORP") & map("PROD")
+println(insect.mkString(" "))
