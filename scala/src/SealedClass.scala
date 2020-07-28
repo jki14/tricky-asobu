53c324275529261f1c02db2f566043fe7242fec1
jki14 <jki14wz@gmail.com>
2020-07-28 23:54:18 +0900

    Play with Scala
    Play with Sealed Class.

--- scala/src/SealedClass.scala
+++ scala/src/SealedClass.scala
@@ -3,3 +3,10 @@ sealed class Request
 final case class Delete(key: String) extends Request
 final case class Insert(key: String, value: String) extends Request
 final case class Select(key: String) extends Request
+
+val request: Request = Insert("key", "value")
+request match {
+    case Delete(key) => println(s"- $key")
+    case Insert(key, value) => println(s"+ $key $value")
+    case _ => println("Not Implemented")
+}
