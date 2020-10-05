3abf3360d877ccd33ff59d0736ed91728d4666ab
jki14 <jki14wz@gmail.com>
2020-10-05 22:59:49 +0900

    Play keyboard.wait with python3 in OSX
    For this moment, the left shift can not trigger wait shift, but the right one
    can.
    Consider using Karabiner to swap them for working around.

---
+++ python/keyboard-wait.py
@@ -0,0 +1,14 @@
+from keyboard import wait
+
+
+def main():
+    wait('j')
+    print('done')
+    wait('shift')
+    print('done')
+    wait('shift+e')
+    print('done')
+
+
+if __name__ == '__main__':
+    main()
