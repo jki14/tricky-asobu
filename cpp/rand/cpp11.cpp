ada015fc8cd409359dee47e2a5210fba8ae2871f
jki14 <jki14wz@gmail.com>
2021-04-24 22:47:32 +0900

    Continue playing with c++11 random
    Explicitly sepcify IntType for std::uniform_int_distribution.

--- cpp/rand/cpp11.cpp
+++ cpp/rand/cpp11.cpp
@@ -4,7 +4,7 @@
 int main() {
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<> distrib(0, 9);
+  std::uniform_int_distribution<int> distrib(0, 9);
   for (int i = 0; i < 32; ++i) {
     std::cout << distrib(gen) << std::endl;
   }
