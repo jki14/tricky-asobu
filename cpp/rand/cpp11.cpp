fa29c96b5037f2f172ca216a5211aad4cdf4424f
jki14 <jki14wz@gmail.com>
2021-04-29 22:14:56 +0900

    Continue playing with c++11 random
    Check the random_device min/max value.

--- cpp/rand/cpp11.cpp
+++ cpp/rand/cpp11.cpp
@@ -3,6 +3,8 @@
 
 int main() {
   std::random_device rd;
+  std::cout << "rd.max() = " << rd.max() << std::endl;
+  std::cout << "rd.min() = " << rd.min() << std::endl;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<int> distrib(0, 9);
   for (int i = 0; i < 32; ++i) {
