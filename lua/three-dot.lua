b13eb5deaadb5a44b3e8cdd2414580b119c23ff4
jki14 <jki14wz@gmail.com>
2021-07-27 20:51:39 +0900

    Play Lua with three-dots
    Test ... for function arguments.

---
+++ lua/three-dot.lua
@@ -0,0 +1,20 @@
+function f(a, b, ...)
+    print('---')
+    print(a or 'nil')
+    print(b or 'nil')
+    foo = table.pack(...)
+    bar = '['
+    for i = 1, #foo do
+        if string.len(bar) > 1 then
+            bar = bar .. ', '
+        end
+        bar = bar .. foo[i] or 'nil'
+    end
+    bar = bar .. ']'
+    print(bar)
+end
+
+f('a')
+f('a', 'b')
+f('a', 'b', 'c')
+f('a', 'b', 'c', 'd')
