6644e7d3173a6674419a43d12bf7a4cdc3b7df06
jki14 <jki14wz@gmail.com>
2021-08-25 20:51:52 +0900

    Play with c++ external equality
    Check the external equality operator in c++.

---
+++ cpp/experimental/class/equality.cpp
@@ -0,0 +1,17 @@
+struct C {
+public:
+  int x, y;
+};
+
+inline bool operator==(C const& lhs, C const& rhs) {
+  return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+int main() {
+  C lhs, rhs;
+  lhs.x = 11;
+  lhs.y = 14;
+  rhs.x = 11;
+  rhs.y = 14;
+  return !(lhs == rhs);
+}
