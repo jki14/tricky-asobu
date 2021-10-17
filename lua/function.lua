be7f8153c936769d5bc1210e79c736d6ddc279cc
jki14 <jki14wz@gmail.com>
2021-10-17 20:13:34 +0900

    Play with Lua function optional parameters
    Test partial function parameters passing.

---
+++ lua/function.lua
@@ -0,0 +1,10 @@
+function func(x, y, z)
+    print('x = ' .. tostring(x))
+    print('y = ' .. tostring(y))
+    print('z = ' .. tostring(z))
+    print('\n')
+end
+
+func(1, 2, 3)
+func(1, 2)
+func(1)
