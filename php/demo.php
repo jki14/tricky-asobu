6a24a87e8578aebb37a7350660d1f7d07fbd3ea2
jki14 <jki14wz@gmail.com>
2022-03-06 23:10:19 +0900

    Play PHP Programing Language
    Demonstrate duplicate key dictionary JSON decode and sha1.

--- php/demo.php
+++ php/demo.php
@@ -13,3 +13,18 @@
 
     define('USERID', 'userid');
     echo USERID.PHP_EOL;
+
+    $jsonstr = '{
+      "key0": "value0",
+      "key1": "value1",
+      "key1": "value2"
+    }';
+
+    $foo = json_decode($jsonstr, true);
+    echo $foo['key1'].PHP_EOL;
+
+    $foo = 'master';
+    $foo = sha1($foo.'tgtoken');
+    echo $foo.PHP_EOL;
+    $bar = sha1('tgtoken'.$foo);
+    echo $bar.PHP_EOL;
