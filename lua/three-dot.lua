6132c268285e8393d464b2376b9ba848d63be566
jki14 <jki14wz@gmail.com>
2022-04-25 23:14:19 +0900

    Play Lua with three dot select
    Demonstrate that three-dot isn't replaceable easily in Lua Programming
    Language.

--- lua/three-dot.lua
+++ lua/three-dot.lua
@@ -20,3 +20,26 @@ f('a')
 f('a', 'b')
 f('a', 'b', 'c')
 f('a', 'b', 'c', 'd')
+
+function g(a, b, ddd)
+    print('---')
+    print(a or 'nil')
+    print(b or 'nil')
+    -- foo = table.pack(ddd)
+    foo = {ddd}
+    bar = '['
+    for i = 1, #foo do
+        if string.len(bar) > 1 then
+            bar = bar .. ', '
+        end
+        bar = bar .. foo[i] or 'nil'
+    end
+    bar = bar .. ']'
+    print('select(1, ddd) = ' .. (select(1, ddd) or 'nil'))
+    print(bar)
+end
+
+g('a')
+g('a', 'b')
+g('a', 'b', 'c')
+g('a', 'b', 'c', 'd')
